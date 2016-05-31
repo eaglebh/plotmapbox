86ab7e4b5ed2523a373b39009acd12b3c5962f86
Pablo Marcondes Fonseca <pablo.mmarcondes@gmail.com>
2016-05-30 23:17:54 -0300

    changes to mapbox v4 and removal of marker feature

--- R/getmapbox_map.R
+++ R/getmapbox_map.R
@@ -1,20 +1,20 @@
 #' Get mapbox map
-#' \code{getmapbox_map} uses the Mapbox Static API to get map images 
-#' 
+#' \code{getmapbox_map} uses the Mapbox Static API to get map images
+#'
 #' @param center a latitude and longitude vector c(lat,lng)
 #' @param mapbox the mapbox map to display, default to example from mapbox
 #' @param zoom the map zoom stage (1-21)
 #' @param size a width and height vector c(width,height)
 #' @param filename the name for the saved map
-#' @param markers a list of markers 
+#' @param access the access token
 #' @param ... arguments for ggmap and arguments for get_mapboxmap
 #' @return a list of meta information (filename,center,zoom,size)
 #' @seealso \code{\link{ggmap}}
 
-getmapbox_map = function(center=c(),mapbox='examples.map-vyofok3q',zoom=13,size=c(640,480),filename='map.png',markers=list()) {
-  url = paste("http://api.tiles.mapbox.com/v3/",mapbox,mapbox_marker(markers),'/',center[1],',',center[2],',',zoom,'/',size[1],'x',size[2],'.png',sep="")
-  ret = download.file(url, filename, mode="wb", quiet = FALSE)
-  map_meta = list(filename=filename,center=center,zoom=zoom,size=size)
-  
-  return(map_meta)
+getmapbox_map = function(center=c(), mapbox='mapbox.light', zoom=14, size=c(1280, 1280), filename='map.png', access = NULL) {
+    url = paste("http://api.tiles.mapbox.com/v4/",mapbox,'/',center[1],',',center[2],',',zoom,'/',size[1],'x',size[2],'.png?access_token=',access,sep="")
+    ret = download.file(url, filename, mode="wb", quiet = FALSE)
+    map_meta = list(filename=filename,center=center,zoom=zoom,size=size)
+
+    return(map_meta)
 }
--- R/mapbox.R
+++ R/mapbox.R
@@ -1,25 +1,22 @@
 #' Quick mapbox mapping
 #' \code{mapbox} uses ggmap to plot a mapbox static map
-#' 
+#'
 #' @param center a latitude and longitude vector c(lat,lng)
 #' @param mapbox the mapbox map to display, default to example from mapbox
 #' @param ... arguments for ggmap and arguments for get_mapboxmap
 #' @return a ggmap object
 #' @seealso \code{\link{ggmap}}
 #' @examples
-#' \dontrun{ 
+#' \dontrun{
 #' getmap(c(-77.04, 38.89))
 #' #set the zoom
 #' getmap(c(-77.04, 38.89),zoom=14)
 #' #setmapbox map
-#' getmap(c(-77.04, 38.89),mapbox="examples.map-vyofok3q")
+#' getmap(c(-77.04, 38.89),mapbox="mapbox.light")
 #' #set extent for ggmap
-#' getmap(c(-77.04, 38.89),mapbox="examples.map-vyofok3q",extent='devise')
-#' #add markers to the map
-#' marker = c(size='m',icon='a',latlng='-77,38')
-#' markers = list(marker)
-#' getmap(c(-77.04, 38.89),mapbox="examples.map-vyofok3q",markers=markers)
+#' getmap(c(-77.04, 38.89),mapbox="mapbox.light",extent='devise')
+#' getmap(c(-77.04, 38.89),mapbox="mapbox.light")
 #' }
-mapbox = function(center=c(-77.04,38.89),mapbox='examples.map-vyofok3q',...) {
+mapbox = function(center=c(-77.04,38.89),mapbox='mapbox.light',...) {
   map_png(getmapbox_map(center,mapbox,...),...)
 }
